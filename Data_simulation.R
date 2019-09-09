550da522d7c553da643b34ca9c91744de78fec7c
duarte-viana <33447038+duarte-viana@users.noreply.github.com>
2019-09-09 14:23:25 +0200

    Update Data_simulation.R

--- Data_simulation.R
+++ Data_simulation.R
@@ -1,5 +1,5 @@
 ###########################################
-# Supporting Information, Appendix S3
+# Supporting Information
 # "Partitioning environment and space in species-by-site matrices: a comparison of methods for community ecology and macroecology"
 # Duarte S. Viana, Petr Keil, Alienor Jeliazkov
 ###########################################
@@ -18,10 +18,13 @@ library(rgeos)
 library(adespatial)
 library(plotrix)
 
-# Load table of parameters
+# Load parameters
 load("pars_GB.Rdata")
 
-# Define landscape (lattice)
+# Define type of data ("counts" or "binary")
+dat <- "binary" 
+
+# Define lattice
 edge <- floor(pars$Nsamples[task.id]/2)
 grid.side <- pars$Nsamples[task.id] + edge*2
 grid.size<-grid.side^2
@@ -31,10 +34,14 @@ gridded(xy) = ~x+y
 coords<-coordinates(xy)
 coords2<-as.data.frame(coords)
 
-# Autocorrelated environment (Es)
-g.dummy <- gstat(formula=z~1, locations=~x+y, dummy=T, beta=1, 
-                 model=vgm(psill=0.025,model="Exp",range=pars$range[task.id]), nmax=20)
-# make 1 simulation based on the gstat object
+# Autocorrelated environment
+type <- "mosaic"
+if(type=="mosaic") g.dummy <- gstat(formula=z~1, locations=~x+y, dummy=T, beta=1, 
+                                    model=vgm(psill=0.025,model="Exp",range=pars$range[task.id]), nmax=20)
+# trend in x and y
+if(type=="gradient") g.dummy <- gstat(formula=z~1+x+y, locations=~x+y, dummy=T, 
+                                      beta=c(1,0.01,0.01), model=vgm(psill=0.025, range=pars$range[task.id], model='Exp'), nmax=20)
+# make 1 simulation based on the stat object
 yy <- predict(g.dummy, newdata=xy, nsim=1)
 # Transform environmental variable from normal to uniform
 ecum1<-ecdf(yy@data[,1])
@@ -43,12 +50,12 @@ yy@data[,1]<-ecum.yy
 yy.data<-yy@data[,1]
 env1<-yy.data
 
-# Randomly distributed environment (Er)
+# Randomly distributed environment
 yy2<-SpatialPixelsDataFrame(coordinates(xy),as.data.frame(runif(grid.size,0,1)))
 env2 <- yy2@data[,1]
 
 # Distribute species - calculate abundance for each species in each cell according to environment
-# Niche component (lambda)
+# Niche component
 R <- pars$R[task.id]
 opt <- seq(0.05,0.95,length.out=R)
 mat1 <- matrix(ncol=R,nrow=grid.size)
@@ -58,7 +65,7 @@ for(i in 1:R){
   mat1[,i] <- 0.75*fe1 + 0.25*fe2
 }
 
-# Dispersal component (D)
+# Dispersal component
 matd <- matrix(ncol=R,nrow=grid.size)
 for(i in 1:R) matd[,i] <- rlnorm(nrow(matd),1,0.5)
 neighb <- edge
@@ -73,7 +80,7 @@ for(i in 1:grid.size){
   jnei <- wini[wnei]
   dists <- dists[wnei]
   for(j in 1:R){
-    mat2[i,j] <- sum(matd[jnei,j]*(exp((dists*-1)/0.5))) # weight cells by dispersal kernel
+    mat2[i,j] <- sum(matd[jnei,j]*(exp((dists*-1)/0.5)))
   }
 }
 
@@ -87,21 +94,31 @@ mat.env <- cbind(env1,env2)
 mat.env0 <- mat1[crop,]
 mat.spa <- mat2[crop,]
 
-# Scale components and rescale to range from 0 to 20 (abundance)
+# Scale components
 mat.spa <- mat.spa*sum(mat.env0)/sum(mat.spa)
 mat.spa <- scale(mat.spa)
 mat.env0 <- scale(mat.env0)
-mat.spa <- rescale(mat.spa,c(0,20))
-mat.env0 <- rescale(mat.env0,c(0,20))
 
 # Calculate species abundance - build species matrix
 ws <- pars$ws[task.id]
 we <- 1-ws
-mean.abund <- as.vector(ws*mat.spa + we*mat.env0)
-final.abund <- rpois(length(mean.abund),mean.abund)
-mat.sp <- matrix(final.abund,nrow=nrow(mat.env0),ncol=ncol(mat.env0))
 
-# Calculate MEMs from cell coordinates
+if(dat=="counts"){
+  mat.spa <- rescale(mat.spa,c(0,20))
+  mat.env0 <- rescale(mat.env0,c(0,20))
+  mean.abund <- as.vector(ws*mat.spa + we*mat.env0)
+  final.abund <- rpois(length(mean.abund),mean.abund)
+  mat.sp <- matrix(final.abund,nrow=nrow(mat.env0),ncol=ncol(mat.env0))
+}
+
+if(dat=="binary"){
+  mean.abund <- as.vector(ws*mat.spa + we*mat.env0)
+  pr <- 1/(1+exp(-mean.abund)) # pass through an inv-logit function
+  final.pr <- rbinom(length(mean.abund),1,pr)
+  mat.sp <- matrix(final.pr,nrow=nrow(mat.env0),ncol=ncol(mat.env0))
+}
+
+# PCNM
 xy.pcnm <- dbmem(xyd,MEM.autocor = "positive",silent=TRUE)
 all.pcnm <- as.data.frame(xy.pcnm)
 
